ddc395ebee20d17eaa17853fe221688500cbfa10
gundo <gundo.tshilimandila@wundermanthompson.com>
2022-10-17 20:22:21 +0200

    rem jwt

--- autotaxi/settings.py
+++ autotaxi/settings.py
@@ -177,7 +177,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-        'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     ]
 }
 
